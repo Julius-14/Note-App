6874c4f7b4fc2acccbf1c9d221f206d070d04962
julius_gando <juliusgando2@gmail.com>
2022-10-20 14:34:08 +0800

    update.js added

---
+++ update.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+const update = function(note, oldNote) {
+    
+    const oNote = JSON.parse(oldNote)
+
+    const newNote = oNote.map(n => {
+        if(n.id == note.id){
+           return { ...n, title: note.title, body: note.body}
+    }
+    return n
+})
+
+fs.writeFileSync('./note.txt', JSON.stringify(newNote))
+console.log(newNote)
+
+}
+module.exports = update
